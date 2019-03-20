2d40df05a260ae1ba0f3381f0b24506948d82a27
Joseph Scott <jbs198585@gmail.com>
2019-03-20 07:20:36 -0400

    second

---
+++ src/com/joescott/Main.java
@@ -0,0 +1,23 @@
+package com.joescott;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Joe", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore(100, 100);
+
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player " + "scored " + score + " points");
+        return score * 1000;
+    }
+
+}
