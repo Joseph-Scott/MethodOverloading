237ab03f00b63aa35b94b3f591174153fc748b44
Joseph Scott <jbs198585@gmail.com>
2019-03-21 09:41:06 -0400

    challenge

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -8,6 +8,24 @@ public class Main {
         calculateScore(75);
         calculateScore();
 
+        // Create a method called calcFeetAndInchesToCentimeters
+        // It needs to have two parameters.
+        // feet is the first parameter, inches is the 2nd parameter
+        //
+        // You should validate that the first parameter feet is >= 0
+        // You should validate that the 2nd parameter inches is >= 0 and <= 12
+        // return -1 from the method if either of the above is not true
+        //
+        // If the parameters are valid, then calculate how many centimeters
+        // comprise the feet and inches passed to this method and return
+        // that value.
+        //
+        // Create a 2nd method of the same name but with only one parameter
+        // inches is the parameter
+        //
+
+        }
+
     }
 
     public static int calculateScore(String playerName, int score) {
