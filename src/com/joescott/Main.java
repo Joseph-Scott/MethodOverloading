a090562a29f365bcef5d5249a31ee17998e6c2c6
Joseph Scott <jbs198585@gmail.com>
2019-03-22 06:36:09 -0400

    test

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        double centimeters = calcFeetAndInchesToCentimeters(6, 0);
+        if (centimeters < 0.0) {
+            System.out.println("Invalid parameters");
+        }
 
         // Create a method called calcFeetAndInchesToCentimeters
         // It needs to have two parameters.
@@ -22,11 +26,47 @@ public class Main {
         //
         // Create a 2nd method of the same name but with only one parameter
         // inches is the parameter
+        // validate that its >= 0
+        // return -1 if it is not true
+        // But if its valid, then calculate how many feet are in the inches
+        // and then here is the trick part
+        // call the other overloaded method passing the correct feet and inches
+        // calculated so that it can calculate correctly.
         //
+        // Hints: Using double for your number datatypes is probably a good idea
+        // 1 inch = 2.54cm and 1 foot = 12 inches
+        // Use the link I give you to confirm your code is calculating correctly.
+        // Calling another overloaded method just requires you to use the right number of parameters.
+    }
 
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+
+        if ((feet < 0) || ((inches < 0) || (inches > 12))) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
         }
 
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points");
